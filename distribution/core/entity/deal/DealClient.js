4711b8336b48032262d8379ad9b487d9845fa796
Áxel Kovacki <axelkovacki7@gmail.com>
2021-11-07 13:16:10 -0300

    add distribution folder to .gitignore

--- distribution/core/entity/deal/DealClient.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var DealClient = /** @class */ (function () {
-    function DealClient(name) {
-        this.name = name;
-    }
-    return DealClient;
-}());
-exports.default = DealClient;
